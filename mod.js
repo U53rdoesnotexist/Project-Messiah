ca84fbb7a6b06529facbf4a6806e70303c1b7952
Teinc3 <s17415@lsc.hk>
2023-03-20 03:23:09 +0800

    Messiah

--- mod.js
+++ mod.js
@@ -1,39 +1,39 @@
-var modHandler, latencySimulator, spawnHider;
+var modHandler, latencySimulator, spawnHider, messiah;
 function modConstruct(){
     modHandler = new ModHandler;
     latencySimulator = new LatencySimulator;
     spawnHider = new SpawnHider;
+    messiah = new Messiah;
 }
 
 function ModHandler() {
-    var cycle, tick, spawnTick;
+    this.cycle, this.tick;
     this.font = true;
     this.latency = true;
     this.hideSpawn = true;
+    this.messiah = true;
     this.scriptGameInit = function() {
-        cycle = 1;
-        tick = 0;
-        if (!singleplayer) {
-            spawnTick = 0;
-            spawnHider.init();
-        }
+        this.cycle = 1;
+        this.tick = 0;
+        if (!singleplayer) spawnHider.init();
+        if (this.messiah) messiah.init();
     };
     this.scriptSpawnTick = function() {
-        spawnTick++;
-        if (modHandler.hideSpawn) spawnHider.setSpawn(spawnTick)
+        if (modHandler.hideSpawn) spawnHider.setSpawn(mainHandler.multiplayerHandler.packetsReceived)
     };
     this.scriptGameTick = function() {
-        tick++;
-        if (tick >= 100) {
-            cycle++;
-            tick %= 100;
+        this.tick++;
+        if (this.tick >= 100) {
+            this.cycle++;
+            this.tick %= 100;
         }
+        if (this.messiah) messiah.update()
     };
     this.density = function(id) {
         return troops[id] / land[id]
     };
-    this.distance = function(xCoord,yCoord) {
-        return (xCoord**2 + yCoord**2) ** 0.5
+    this.getDistance = function(xOffset, yOffset) {
+        return (xOffset**2 + yOffset**2) ** 0.5
     };
     this.getSpeed = function(id) {
         return land[id] < 1E3 ? 1 / 4 : land[id] < 1E4 ? 1 / 3 : land[id] < 6E4 ? 1 / 2 : land[id] < 16E4 ? 1 : land[id] < 32E4 ? 2 : 3
@@ -71,6 +71,7 @@ function LatencySimulator() {
 }
 
 function SpawnHider() {
+    var revealTick = 3;
     this.init = function() {
         this.decoySpawn = new Uint16Array(2);
         this.chosenSpawn = new Uint16Array(2);
@@ -90,7 +91,7 @@ function SpawnHider() {
                 this.chosenSpawn[0] = xCoord;
                 this.chosenSpawn[1] = yCoord;
             }
-            if (mainHandler.multiplayerHandler.packetsReceived >= Math.floor(.85 * spawnTime) && this.decoySpawn[0] != 0 && this.decoySpawn[1] != 0) {
+            if (mainHandler.multiplayerHandler.packetsReceived >= spawnTime - revealTick && this.decoySpawn[0] != 0 && this.decoySpawn[1] != 0) {
                 dataEncoder.setLocation(1E3, this.decoySpawn[0], this.decoySpawn[1]);
             }
         }
@@ -98,8 +99,82 @@ function SpawnHider() {
     this.setSpawn = function(spawnTick) {
         if (spawnTick == spawnTime && this.chosenSpawn[0] != 0 && this.chosenSpawn[1] != 0) {
             dataEncoder.setLocation(1E3, this.chosenSpawn[0], this.chosenSpawn[1]);
-        } else if (spawnTick == Math.floor(.85 * spawnTime) && this.decoySpawn[0] != 0 && this.decoySpawn[1] != 0) {
+        } else if (spawnTick == spawnTime - revealTick && this.decoySpawn[0] != 0 && this.decoySpawn[1] != 0) {
             dataEncoder.setLocation(1E3, this.decoySpawn[0], this.decoySpawn[1]);
         }
+    }
+}
+
+function Messiah() {
+    function opening() {
+        var ratio = 0;
+        const timings = (!singleplayer || modHandler.latency) ? [64, 69, 67, 65, 63] : [71, 76, 74, 72, 70],
+            ratios = [282, 345, 321, 307, 320];
+        if (modHandler.tick == timings[modHandler.cycle - 1] - (this.opponent != null && !this.opponent.getDistance) ? 7 : 0) ratio = ratios[modHandler.cycle - 1];
+        //If Touching bot, reduce send percentage or do earlier depend on coverage?
+        if (ratio) doAttack(maxEntities, ratio);
+    }
+    function continuousExpansion() {
+
+    }
+    function micro() {
+
+    }
+    function doAttack(targetID, ratio) {
+        if (singleplayer) {
+            if (modHandler.latency) latencySimulator.addPendingAction(0, ratio, targetID, 0, 0)
+            else processAttack(myID, targetID, ratio)
+        } else dataEncoder.attack(ratio, targetID === maxEntities ? myID : targetID)
+    }
+    this.borderingLandPixels;
+    this.borderingBots;
+    this.opponent = null;
+    this.pending = [];
+    this.init = function() {
+        if (playerCount == 2) {
+            this.opponent = {
+                id: 1 - myID,
+                distance: modHandler.getDistance(currentMapWidth, currentMapHeight) - 1
+            }
+        }
+        this.pending = [];
+        this.borderingBots = new Array(512);
+        this.borderingLandPixels = new Array(512);
+        for (let idIndex of this.borderingLandPixels) this.borderingLandPixels[idIndex] = new Array();
+    }
+    this.update = function() {
+        this.borderingLandPixels[myID] = new Array();
+        this.borderingBots[myID] = new Array();
+        if (this.opponent != null && latencySimulator.getNextUpdateTick(modHandler.tick)) {
+            this.borderingLandPixels[this.opponent.id] = new Array()
+            this.borderingBots[this.opponent.id] = new Array();
+        }
+        for (var pIndex1 of landBorderPixels[myID]) {
+            //Update My Own Border Info
+            for (var side = 3; side >= 0; side--) {
+                var pIndex2 = pIndex1 + offset[side];
+                if (!pixel.strongIsOwner(myID, pIndex2) && !this.borderingLandPixels[myID].includes(pIndex2)) this.borderingLandPixels[myID].push(pIndex2);
+                var owner = pixel.getOwner(pIndex2)
+                if (pixel.entityControlled(pIndex2) && owner >= playerCount && !this.borderingBots[myID].includes(owner)) this.borderingBots[myID].push(owner)
+            }
+            //Update Opponent Border Info and Find Min Distance
+            if (this.opponent != null && modHandler.tick % 5 == 1) {
+                for (var pIndex2 of landBorderPixels[this.opponent.id]) {
+                    if (this.opponent.distance) this.opponent.distance = Math.min(modHandler.getDistance(pixel.toX(pIndex1) - pixel.toX(pIndex2), pixel.toY(pIndex1) - pixel.toY(pIndex2)) - 1, this.opponent.distance);
+                    if (!this.borderingLandPixels[this.opponent.id].length) {
+                        for (var side = 3; side >= 0; side--) {
+                            var pIndex3 = pIndex2 + offset[side];
+                            if (!pixel.strongIsOwner(this.opponent.id, pIndex3) && !this.borderingLandPixels[this.opponent.id].includes(pIndex3)) this.borderingLandPixels[this.opponent.id].push(pIndex3);
+                            var owner = pixel.getOwner(pIndex3)
+                            if (pixel.entityControlled(pIndex3) && owner >= playerCount && !this.borderingBots[this.opponent.id].includes(owner)) this.borderingBots[this.opponent.id].push(owner)
+                        }
+                    }
+                }
+            }
+        }
+
+        if (modHandler.cycle <= 5) opening();
+        else if (modHandler.cycle <= 9) continuousExpansion();
+        if (modHandler.cycle >= 7) micro();
     }
 }
